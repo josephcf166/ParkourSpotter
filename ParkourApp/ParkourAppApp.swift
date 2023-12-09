6325658e8dcf44ba1620b4466d65c6837408ded7
j2-cauvyfoster <joseph2.cauvy-foster@live.uwe.ac.uk>
2023-12-09 18:49:57 +0000

    Initial Commit

---
+++ ParkourApp/ParkourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkourAppApp.swift
+//  ParkourApp
+//
+//  Created by joseph cauvy-foster on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParkourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
